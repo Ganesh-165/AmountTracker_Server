034fde8c5d6095b7d4e4e6660124ebca69320e75
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-17 20:48:50 +0530

    Update verifyJWT.js

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 exports.verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
+    console.log(authHeader);
     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
     jwt.verify(
