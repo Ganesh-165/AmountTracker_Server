542edfd3d426d90f7b2f2977f9f4211d52655d4d
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-24 13:29:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,15 @@ const cookieParser = require('cookie-parser');
 
 const connection = require('./database/connection');
 const router = require('./router/routes');
-// const { credentials } = require('./credentials/credential');
-// const { corsOptions } = require('./credentials/corsOptions');
+const { credentials } = require('./credentials/credential');
+const { corsOptions } = require('./credentials/corsOptions');
 require('dotenv').config();
 
 const app = express(); 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://amounttracker-client.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 connection();
-// app.use(credentials)
-// app.use(cors(corsOptions));
+app.use(credentials)
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyparser.urlencoded({extended:false}),express.json());
 app.use(router);
