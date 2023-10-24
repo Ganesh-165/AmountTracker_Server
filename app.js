d6598d590a59aab8e034cbe437355f0c36a47680
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-24 13:25:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const { corsOptions } = require('./credentials/corsOptions');
 require('dotenv').config();
 
 const app = express(); 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://resorse-hub-client.onrender.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 connection();
 app.use(credentials)
