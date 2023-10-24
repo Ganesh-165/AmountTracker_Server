a491f3b7d54300b06ea6445321ec7888d9eaec94
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-10-24 13:26:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const cookieParser = require('cookie-parser');
 
 const connection = require('./database/connection');
 const router = require('./router/routes');
-const { credentials } = require('./credentials/credential');
-const { corsOptions } = require('./credentials/corsOptions');
+// const { credentials } = require('./credentials/credential');
+// const { corsOptions } = require('./credentials/corsOptions');
 require('dotenv').config();
 
 const app = express(); 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://resorse-hub-client.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://amounttracker-client.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 });
 
 connection();
-app.use(credentials)
-app.use(cors(corsOptions));
+// app.use(credentials)
+// app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyparser.urlencoded({extended:false}),express.json());
 app.use(router);
